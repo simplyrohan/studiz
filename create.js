93a1e1d402a00212a7e9c18f9b308196711273fe
Rohan Gupta <simplyrohan10@gmail.com>
2024-11-23 18:13:48 -0800

    change save system

--- create.js
+++ create.js
@@ -12,12 +12,23 @@ flashcardData = {
     'flashcards': []
 }
 
-function addFlashcard(card) {
+function getChildIndex(element) {
+    let index = 0;
+    while (element.previousElementSibling) {
+        element = element.previousElementSibling;
+        index++;
+    }
+    return index;
+}
+
+function addFlashcard(card, index = -1) {
 
     let term = card.querySelector('.flashcard-term').value
     let def = card.querySelector('.flashcard-def').value
 
-    if (term && def) {
+    if (index != -1) {
+        flashcardData.flashcards.splice(index, 0, { term, def })
+    } else {
         flashcardData.flashcards.push({ term, def })
     }
 
@@ -39,25 +50,68 @@ finishSet.addEventListener('click', finish)
 addCard.addEventListener('click', function (e) {
     e.preventDefault();
 
+    let saved = false
+
+    function save() {
+        console.log('saving')
+        let i = getChildIndex(flashcard)
+        // flashcardData.flashcards.pop(i)
+        console.log(i)
+        console.log(JSON.stringify(flashcardData, null, 2))
+        flashcardData.flashcards.splice(i, 1)
+        console.log(JSON.stringify(flashcardData, null, 2))
+        addFlashcard(flashcard, i)
+    }
+
     let flashcardTerm = textarea(
-        { class: 'flashcard-attr flashcard-term', placeholder: 'Flashcard Term' }
+        {
+            class: 'flashcard-attr flashcard-term', placeholder: 'Flashcard Term', oninput: (e) => {
+                // replace the current flashcard with a new one
+                save()
+            }
+        }
     );
     let flashcardDef = textarea(
-        { class: 'flashcard-attr flashcard-def', placeholder: 'Flashcard Definition' }
+        {
+            class: 'flashcard-attr flashcard-def', placeholder: 'Flashcard Definition', oninput: (e) => {
+                // replace the current flashcard with a new one
+                save()
+            }
+        }
     )
 
     let trashButton = button(
-        { class: 'secondary trash', onclick: (e) => { e.preventDefault(); flashcard.remove() } },
+        {
+            class: 'secondary trash', onclick: (e) => {
+                e.preventDefault();
+                flashcardData.flashcards.splice(getChildIndex(flashcard), 1);
+                flashcard.remove()
+
+            }
+        },
         img(
             { src: 'icons/trash.svg' }
         )
     )
-    let addButton = button(
-        { class: 'secondary plus', onclick: (e) => { e.preventDefault(); addFlashcard(flashcard) } },
-        img(
-            { src: 'icons/plus.svg' }
-        )
-    )
+    // let addButton = button(
+    //     {
+    //         class: 'secondary plus', onclick: (e) => {
+    //             e.preventDefault();
+    //             // replace the current flashcard with a new one
+    //             flashcardData.flashcards.pop(getChildIndex(flashcard))
+    //             addFlashcard(flashcard)
+
+    //             saved = true;
+
+    //             // replace the first child with a checkmark
+
+    //             addButton.firstChild.src = 'icons/check.svg'
+    //         },
+    //     },
+    //     img(
+    //         { src: 'icons/plus.svg' }
+    //     )
+    // )
 
     let flashcard = div(
         { class: 'flashcard' },
@@ -68,10 +122,11 @@ addCard.addEventListener('click', function (e) {
         ),
         span(
             { 'class': 'actions' },
-            addButton,
             trashButton
         )
     )
 
     flashcards.appendChild(flashcard)
+
+    addFlashcard(flashcard)
 })
